a6b23deaaf38c78bf952f0b65edba037c6e7c308
steffkes <stefan@gelungen.es>
2026-02-09 08:51:48 +0100

    explain competition details

--- app.vue
+++ app.vue
@@ -131,7 +131,7 @@ useHead({
             <a
               href="https://www.paypal.com/ncp/payment/M78QDRMBT5752"
               class="button is-success is-block"
-              >Ich will mein Early-Bird-Ticket haben!</a
+              >Ich will unser Early-Bird-Ticket haben!</a
             >
           </p>
 
@@ -162,6 +162,24 @@ useHead({
             Für alle, die zu lange gezögert haben und jetzt merken, dass sie
             doch dabei sein wollen. Kurz überlegen, buchen, starten. Reicht dann
             auch.
+          </p>
+
+          <h2>Unser Wettkampf</h2>
+
+          <p>
+            Was in Berlin geht, geht auch in München. Gelaufen wird ein
+            Feuerwehr-Treppenlauf im Zweiertrupp. Entweder angeschlossen oder –
+            falls du deiner Lunge nicht vollständig vertraust – auch nicht.
+            Ansonsten gilt: klassische Einsatzmontur. Helm, Jacke, Handschuhe,
+            Hose und stabile Stiefel. Kurz gesagt alles, was man auch bei der
+            Innenbrandbekämpfung nicht zuhause lässt.
+          </p>
+
+          <p>
+            Gelaufen wird – getreu dem Namen der Veranstaltung – in den
+            Sonnenaufgang hinein. Das bedeutet einen entsprechend frühen Start.
+            Für alle, die morgens noch nicht vollständig einsatzfähig sind, gibt
+            es aber auch spätere Startplätze. Kaffee gibt’s danach. Versprochen.
           </p>
         </div>
       </div>
