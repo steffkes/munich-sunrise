d435a5927afa8ea4ebf5f87a83a9c4842d05746f
steffkes <stefan@gelungen.es>
2026-02-12 19:35:31 +0100

    use page feature

---
+++ pages/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="my-6 px-6">
+    <div class="content">
+      <p>
+        Feuerwehr-Treppenlauf in den Sonnenaufgang. 32 Stockwerke. Alles Glas.
+        Treppenhäuser, Zielbereich – ja, auch die Toiletten. Start, Stufen,
+        Ziel. Oben dann: München. Komplett. Ohne Filter.
+      </p>
+
+      <p>Hast du Bock drauf? Dann jetzt direkt zuschlagen und kaufen:</p>
+
+      <p>
+        <a
+          href="https://www.paypal.com/ncp/payment/M78QDRMBT5752"
+          class="button is-success is-block"
+          >Ich will unser Early-Bird-Ticket haben!</a
+        >
+      </p>
+
+      <p>
+        <strong>Kein Risiko.</strong> Findet der Wettkampf nicht statt, gibt’s
+        dein Geld vollständig zurück. Passt dir der Termin nicht, nimmst du den
+        Startplatz einfach in einem der Folgejahre.
+      </p>
+
+      <h2>Unsere Tickets</h2>
+
+      <p class="has-text-grey-light">
+        Preise jeweils für euer Team à zwei Personen
+      </p>
+
+      <p>
+        <span class="tag"><strong class="mr-1">79€</strong> Früh dran</span>
+        Für alle, die nicht erst Tabellen vergleichen und Wetterberichte lesen.
+        Anmelden, vorbereiten, laufen.
+        <strong>Limitiert auf 50 Plätze – danach ist halt vorbei.</strong>
+      </p>
+      <p>
+        <span class="tag"><strong class="mr-1">89€</strong> Passt scho</span>
+        Für alle, die sich noch denken: „Ja, klingt gut … muss ich mir aber erst
+        noch überlegen.“ München lohnt sich. Der Ausblick auch. Und die Stufen
+        laufen nicht von selbst weg.
+      </p>
+      <p>
+        <span class="tag"><strong class="mr-1">99€</strong> Spät dran</span>
+        Für alle, die zu lange gezögert haben und jetzt merken, dass sie doch
+        dabei sein wollen. Kurz überlegen, buchen, starten. Reicht dann auch.
+        <strong>Ab 25.06.26</strong>
+      </p>
+
+      <h2>Unser Wettkampf</h2>
+
+      <p>
+        Was in Berlin geht, geht auch in München. Gelaufen wird ein
+        Feuerwehr-Treppenlauf im Zweiertrupp. Entweder angeschlossen oder –
+        falls du deiner Lunge nicht vollständig vertraust – auch nicht.
+        Ansonsten gilt: klassische Einsatzmontur. Helm, Jacke, Handschuhe, Hose
+        und stabile Stiefel. Kurz gesagt alles, was man auch bei der
+        Innenbrandbekämpfung nicht zuhause lässt.
+      </p>
+
+      <p>
+        Gelaufen wird – getreu dem Namen der Veranstaltung – in den
+        Sonnenaufgang hinein. Das bedeutet einen entsprechend frühen Start. Für
+        alle, die morgens noch nicht vollständig einsatzfähig sind, gibt es aber
+        auch spätere Startplätze. Kaffee gibt’s danach. Versprochen.
+      </p>
+    </div>
+  </div>
+</template>
