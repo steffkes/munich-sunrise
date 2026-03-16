82a5fc4f55684903061554c38458724df53663f8
steffkes <stefan@gelungen.es>
2026-03-16 13:30:46 +0100

    focus on regular ticket
    early bird period is done since yesterday

--- pages/wettkampf/tickets.vue
+++ pages/wettkampf/tickets.vue
@@ -14,18 +14,25 @@ useSeoMeta({
         Preise jeweils für euer Team à zwei Personen
       </p>
 
-      <p>
+      <p class="has-text-grey-lighter">
         <span class="tag"><strong class="mr-1">79€</strong> Früh dran</span>
         Für alle, die nicht erst Tabellen vergleichen und Wetterberichte lesen.
         Anmelden, vorbereiten, laufen.
-        <strong>Nur bis 15.03.26 – danach ist halt vorbei.</strong>
+        <strong style="color: inherit"
+          >Nur bis 15.03.26 – danach ist halt vorbei.</strong
+        >
       </p>
-      <p>
-        <span class="tag"><strong class="mr-1">89€</strong> Passt scho</span>
-        Für alle, die sich noch denken: „Ja, klingt gut … muss ich mir aber erst
-        noch überlegen.“ München lohnt sich. Der Ausblick auch. Und die Stufen
-        laufen nicht von selbst weg.
-      </p>
+
+      <div class="notification is-success is-light">
+        <p>
+          <span class="tag is-success"
+            ><strong class="mr-1">89€</strong> Passt scho</span
+          >
+          Für alle, die sich noch denken: „Ja, klingt gut … muss ich mir aber
+          erst noch überlegen.“ München lohnt sich. Der Ausblick auch. Und die
+          Stufen laufen nicht von selbst weg.
+        </p>
+      </div>
       <p>
         <span class="tag"><strong class="mr-1">99€</strong> Spät dran</span>
         Für alle, die zu lange gezögert haben und jetzt merken, dass sie doch
