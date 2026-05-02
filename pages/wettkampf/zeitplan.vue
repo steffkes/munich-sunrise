733913f1410014ed1cf11b29932e2d39a0a4f126
steffkes <stefan@gelungen.es>
2026-05-02 21:07:45 +0200

    reflect latest information about potential time table

--- pages/wettkampf/zeitplan.vue
+++ pages/wettkampf/zeitplan.vue
@@ -10,18 +10,26 @@ useSeoMeta({
     <div class="content">
       <h2>Das Zeitliche</h2>
 
-      <p>
-        Gelaufen wird – getreu dem Namen der Veranstaltung – in den
-        Sonnenaufgang hinein. Das bedeutet einen entsprechend frühen Start. Für
-        alle, die morgens noch nicht vollständig einsatzfähig sind, gibt es aber
-        auch spätere Startplätze. Kaffee gibt’s danach. Versprochen.
-      </p>
-
       <div class="notification">
         <p>
           Der Zeitplan ist noch nicht in Stein gemeißelt. Wir arbeiten dran.
         </p>
       </div>
+
+      <h3>Samstag</h3>
+
+      <ul>
+        <li>
+          <span class="tag is-light">05:30-06:00</span> Ausgabe der Startnummern
+        </li>
+        <li><span class="tag is-light">06:30</span> Startschuss erstes Team</li>
+      </ul>
+
+      <p class="mt-6">
+        Im Ziel gibt es ein bisschen Sportler-Verpflegung, anschließend euer
+        Weißwurst-Frühstück (sofern ihr dieses bei der Anmeldung vorbestellt
+        habt).
+      </p>
     </div>
   </div>
 </template>
